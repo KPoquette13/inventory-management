9c1b1784bc0c5913edf2dd572a269832ab8bd979
Kurt Poquette <kxp1607@rit.edu>
2016-05-09 18:54:20 -0400

    Beginning js, api details needed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,55 @@
-$(document).ready(function () {
-	$CURRENTPAGE = $('#inventory-container');
-	$ACTIVEBUTTON =$('#inv-button');
+var queryURL = "vm343a.se.rit.edu/inventory"; //Is this right?
 
+function submitInventory(){
+	//parse table
+
+	//build json array
+
+	//call API
+
+}
+
+function populateLog(result){
+
+
+}
+
+function getLogs(){
+	var finalURL = queryURL + "/logging/all";
+
+	var result = $.ajax({
+		url: finalURL,
+		datatype:'json',
+		type:'GET'
+	})
+	.done(function(result){
+		populateLog(result);
+	});
+}
+
+function populateInventory(result){
+	//Need to see format of data
+}
+
+function getFullInventory(){
+	var finalURL = queryURL + "/all";
+
+	var result = $.ajax({
+		url: finalURL,
+		datatype:'json',
+		type:'GET'
+	})
+	.done(function(result){
+		populateInventory(result);
+	});
+}
+
+
+$(document).ready(function () {
+	var $CURRENTPAGE = $('#inventory-container');
+	var $ACTIVEBUTTON =$('#inv-button');
+
+	// Nav Button Functionality
 	$('#inv-button').click(function() {
 		if(!($(this).hasClass('active-button'))){
 			$CURRENTPAGE.addClass('hidden');
@@ -36,6 +84,21 @@ $(document).ready(function () {
 		}
 	});
 
+	//Inventory Table Population
 
+
+	//Inventory Submission
+
+
+	//Inventory Table Validation
+
+
+	//Order Submission
+
+
+	//Build Inventory Object
+
+
+	//Log table population
 
 });
