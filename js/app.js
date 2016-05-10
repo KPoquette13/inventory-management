24ab36206dff68c908740be847d49ea786ff0d11
Kurt Poquette <kxp1607@rit.edu>
2016-05-10 03:21:21 -0400

    Most everything in, but CORS can suck a big one

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-var queryURL = "vm343a.se.rit.edu/inventory"; //Is this right?
+var queryURL = "http://vm343a.se.rit.edu:8008/inventory";
+var inventoryArray = [];
+var logArray = [];
 
 function submitInventory(){
 	//parse table
@@ -9,13 +11,49 @@ function submitInventory(){
 
 }
 
-function populateLog(result){
+//clone log row template, update each span, add to table
+function updateLogs(){
+	$.each(logArray, function(index, value){
+		var row = $('#log-template').clone().removeAttr('id').removeClass('hidden').addClass('log-row');
 
+		var rowId = row.find('.logId');
+		rowId.text = value.id;
+
+		var rowTransactionId = row.find('.logTransactionId');
+		rowTransactionId.text = value.transactionId;
+
+		var rowDescription = row.find('.logDescription');
+		rowDescription.text = value.description;
+
+		var rowProductId = row.find('.logProductId');
+		rowProductId.text = value.productId;
+
+		var rowAmountChanged = row.find('.logAmountChanged');
+		rowAmountChanged.text = value.amountChanged;
+
+		$('#log-table').append(row);
+	});
+}
+
+//handles building the row objects
+function populateLog(result){
+	$.each(resultArray, function(index, value){
+		var id = value.id;
+		var transactionId = value.transactionId;
+		var description = value.description;
+		var productId = value.productId;
+		var amountChanged = value.amountChanged;
+
+		var rowObj = buildInvObject(id, transactionId, description, productId, amountChanged);
+		console.log(rowObj);
+		logArray.push(rowObj);
+	});
 
 }
 
+//makes api call for json array of log rows
 function getLogs(){
-	var finalURL = queryURL + "/logging/all";
+	var finalURL = queryURL + "/logging";
 
 	var result = $.ajax({
 		url: finalURL,
@@ -24,15 +62,67 @@ function getLogs(){
 	})
 	.done(function(result){
 		populateLog(result);
+		updateLogs();
 	});
 }
 
-function populateInventory(result){
-	//Need to see format of data
+function updateInventory(){
+	$.each(inventoryArray, function(index, value){
+		var row = $('#inv-template').clone().removeAttr('id').removeClass('hidden').addClass('data-row');
+
+		var rowProduct = row.find('.invProduct');
+		rowProduct.text = value.product;
+
+		var rowName = row.find('.invName');
+		rowName.text = value.name;
+
+		var rowAmount = row.find('.invAmount');
+		rowAmount.text = value.amount;
+
+		var rowBasePrice = row.find('.invBasePrice');
+		rowBasePrice.text = value.basePrice;
+
+		var rowBaseCost = row.find('.invBaseCost');
+		rowBaseCost.text = value.baseCost;
+
+		$('#inv-table').append(row);
+	});
+}
+
+//builds inventory row object
+function buildInvObject(product, name, amount, basePrice, baseCost){
+	console.log('got to buildObj');
+	var rowObj = {};
+	rowObj.product = product;
+	rowObj.name = name;
+	rowObj.amount = amount;
+	rowObj.basePrice = basePrice;
+	rowObj.baseCost = baseCost;
+
+	console.log(rowObj);
+	return rowObj;
+}
+
+//for each object in array, build row object and put in array to update table
+function populateInventory(resultArray){
+	console.log('got to PopulateInv');
+	$.each(resultArray, function(index, value){
+		var product = value.product;
+		var name = value.name;
+		var amount = value.amount;
+		var basePrice = value.basePrice;
+		var baseCost = value.baseCost;
+
+		var rowObj = buildInvObject(product, name, amount, basePrice, baseCost);
+		console.log(rowObj);
+		inventoryArray.push(rowObj);
+	});
+
 }
 
 function getFullInventory(){
-	var finalURL = queryURL + "/all";
+	console.log('Got to FullInventory');
+	var finalURL = queryURL + "/products";
 
 	var result = $.ajax({
 		url: finalURL,
@@ -40,7 +130,9 @@ function getFullInventory(){
 		type:'GET'
 	})
 	.done(function(result){
+		console.log(result);
 		populateInventory(result);
+		updateInventory();
 	});
 }
 
@@ -85,20 +177,14 @@ $(document).ready(function () {
 	});
 
 	//Inventory Table Population
-
+	//getFullInventory();
 
 	//Inventory Submission
 
-
 	//Inventory Table Validation
-
 
 	//Order Submission
 
-
-	//Build Inventory Object
-
-
 	//Log table population
-
+	//getLogs();
 });
